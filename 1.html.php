21906ff7d26f9f58e84a879d3543f15da39325cc
lan931009 <candy1oo94o2o@gmail.com>
2025-04-14 20:16:22 +0800

    1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,10 +1,25 @@
 <html>
-    <head><title>html+php</title></head>      
+    <head>
+        <title>html+php</title> <!-- 網頁標題 -->
+    </head>      
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php 
+        // 使用 echo 輸出字串 "Hello world!" 到網頁上
+        echo "Hello world!"; 
+        ?> 
+
+        <h1>html</h1> <!-- 顯示 HTML 的標題 -->
+
+        <?php 
+        // 再次輸出 "Hello world!"，這是第二次顯示
+        echo "Hello world!"; 
+        ?> 
+
+        <h1>html</h1> <!-- 再次顯示 HTML 的標題 -->
+
+        <?php 
+        // 第三次輸出 "Hello world!" 到網頁
+        echo "Hello world!"; 
+        ?> 
     </body>
 </html>
