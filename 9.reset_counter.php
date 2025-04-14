c0f45c69a964f35f868293c69981a975436cb8bd
lan931009 <candy1oo94o2o@gmail.com>
2025-04-14 20:26:42 +0800

    9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,13 @@
 <?php
+    // 啟動 session，讓我們可以使用 $_SESSION 變數來儲存資料
     session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
 
+    // 使用 unset() 函數來刪除 session 中的 "counter" 變數
+    unset($_SESSION["counter"]);
+
+    // 顯示 "counter" 重置成功的訊息
+    echo "counter重置成功....";
+
+    // 使用 meta 標籤進行頁面自動跳轉，2秒後跳回 8.counter.php 頁面
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
 ?>
