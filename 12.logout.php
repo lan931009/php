2d8b0f88225f742a543705a7d13f84331113afeb
lan931009 <candy1oo94o2o@gmail.com>
2025-04-14 20:21:28 +0800

    12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,13 @@
 <?php
+    // 啟用 session 功能，準備操作 $_SESSION 變數
     session_start();
-    unset($_SESSION["id"]);
-    echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 
+    // 移除 $_SESSION["id"]，代表使用者登出
+    unset($_SESSION["id"]);
+
+    // 顯示登出成功訊息
+    echo "登出成功....";
+
+    // 3 秒後導向回登入頁面
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 ?>
